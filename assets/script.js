2afe4f31c2902532f488f03a09509341c303e0b6
juanigomez <88172215+juanigomez@users.noreply.github.com>
2023-11-27 23:32:28 -0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// NavBar - toggle Responsive Menu:
+
+burger = document.querySelector(".burger");
+navBar = document.querySelector(".menu");
+
+burger.onclick = function(){
+    navBar.classList.toggle("active");
+    burger.classList.toggle("active");
+}
+
+navBar.onclick = function(){
+    navBar.classList.toggle("active");
+    burger.classList.toggle("active");
+}
